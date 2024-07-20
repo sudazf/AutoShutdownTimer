15ced7aa284b658b2f5df78c31a0cfb87e799e52
Fei Zhu <zfgogogo@163.com>
2024-07-20 23:17:48 +0800

    优化：防止程序重复运行。

--- MyTools/AutoShutdown/App.xaml.cs
+++ MyTools/AutoShutdown/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace AutoShutdown
@@ -13,5 +9,37 @@ namespace AutoShutdown
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var title = "定时关机小工具";
+            _mutex = new Mutex(true, title, out var isNewInstance);
+
+            if (!isNewInstance)
+            {
+                ActivateOtherWindow(title);
+                Shutdown();
+            }
+            else
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+        }
+
+        private static void ActivateOtherWindow(string title)
+        {
+            var existInstance = Natives.NativeMethods.FindWindow(null, title);
+            if (existInstance != IntPtr.Zero)
+            {
+                Natives.NativeMethods.SetForegroundWindow(existInstance);
+                if (Natives.NativeMethods.IsIconic(existInstance))
+                {
+                    //https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+                    Natives.NativeMethods.ShowWindow(existInstance, 1);
+                }
+            }
+        }
     }
 }
---
+++ MyTools/AutoShutdown/Natives/NativeMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoShutdown.Natives
+{
+    public static class NativeMethods
+    {
+        [DllImport("user32", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string cls, string win);
+
+        [DllImport("user32")]
+        public static extern uint SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", CharSet = CharSet.Auto)]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+
+        [DllImport("user32")]
+        public static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32")]
+        public static extern bool OpenIcon(IntPtr hWnd);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetCursorPos(out POINT pt);
+    }
+}
